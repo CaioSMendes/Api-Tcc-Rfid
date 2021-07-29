958fef2360e2e9b98a0966f58c60b387da52851e
CaioSMendes <caioitallo.souzamendes@gmail.com>
2021-07-29 19:49:36 -0300

    JSON mais apresentavel

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,19 @@
 class Product < ApplicationRecord
     belongs_to :categorie
+
+    def name_categorie
+        self.categorie.name
+    end
+
+    def cod_categorie
+        self.categorie.codCategorie
+    end
+
+    def as_json(options={})
+        super(
+            root: true,
+            methods: [:name_categorie, :cod_categorie]
+        )
+    end
 end
+
