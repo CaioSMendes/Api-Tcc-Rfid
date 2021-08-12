a952080a291e507c55236100d161f0b6001f87d7
CaioSMendes <caioitallo.souzamendes@gmail.com>
2021-08-11 22:47:38 -0300

    add campos devise auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::API
+        before_action :configure_permitted_parameters, if: :devise_controller?
         include DeviseTokenAuth::Concerns::SetUserByToken
         #before_action :authenticate_user! #faz autenticacao login na api
+        
+        protected
+
+        def configure_permitted_parameters
+                devise_parameter_sanitizer.permit(:sign_up, keys:[:companyName, :name, :nickname]) 
+        end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,8 +3,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
 
   # GET /resource/sign_up
   # def new
@@ -40,12 +38,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  protected
+  #protected
 
   # If you have extra params to permit, append them to the sanitizer.
-   def configure_sign_up_params
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute, :companyName])
-   end
+  #def configure_sign_up_params
+    #devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  #end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
