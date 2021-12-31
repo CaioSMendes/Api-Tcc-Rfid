ce74af6c16fbe78a5762c13be7fde6d6d353d147
CaioSMendes <caioitallo.souzamendes@gmail.com>
2021-12-31 14:03:02 -0300

    reduzindo campos cadastro produtos

--- db/migrate/20210729183117_create_products.rb
+++ db/migrate/20210729183117_create_products.rb
@@ -2,19 +2,13 @@ class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
       t.string :name
-      t.text :description
+      t.float :price
       t.integer :quantity
       t.string :unity
-      t.float :price
-      t.date :date
+      t.text :description
       t.string :productCode
       t.string :gtin
       t.string :rfid
-      t.integer :productMin
-      t.integer :productMax
-      t.float :salePrice
-      t.float :saleCost
-      t.string :avaliable
 
       t.timestamps
     end
